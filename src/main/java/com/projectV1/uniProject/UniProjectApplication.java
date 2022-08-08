23917666b90e1d1c5d9e5b643dc5bbfde0dedcfb
Mohamad Dhaini <mohamad.dhaini02@lau.edu>
2022-08-08 10:39:00 +0300

    Started the project

---
+++ src/main/java/com/projectV1/uniProject/UniProjectApplication.java
@@ -0,0 +1,13 @@
+package com.projectV1.uniProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projectV1/uniProject/UniProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectV1.uniProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
